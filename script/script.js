236b24a6db97f1f6b59ec72a97e0890a213d701c
JuanCogollo <cogolloyt@gmail.com>
2024-12-20 18:44:42 -0400

    primer commit

---
+++ script/script.js
@@ -0,0 +1,29 @@
+function showRegisterForm() {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('registerForm').style.display = 'block';
+}
+
+const userForm = document.getElementById('userForm');
+
+userForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Almacenar los datos en localStorage
+    localStorage.setItem(username, JSON.stringify({
+        username: username,
+        password: password
+    }));
+
+    // Redirigir al panel de usuario
+    window.location.href = '../html/panel.html';
+});
+function cerrarSesion() {
+    // Eliminar los datos del usuario del localStorage
+    localStorage.removeItem(username);
+
+    // Redirigir a la página de inicio
+    window.location.href = '../html/index.html';
+}
